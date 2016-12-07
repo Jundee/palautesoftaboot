dcf4b6d1ee9b4448a0fe0aa3823d83d74275b3ee
Juntunen Lauri <a1402993@myy.haaga-helia.fi>
2016-12-07 10:15:57 +0200

    vitusti erroreita

--- src/main/java/fi/haagahelia/Application.java
+++ src/main/java/fi/haagahelia/Application.java
@@ -1,8 +1,13 @@
 package fi.haagahelia;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class Application {
 
